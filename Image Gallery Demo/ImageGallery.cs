a744f27e0071f09d7115e94a193272c448d9f5f1
ankit <ankitjadli26@gmail.com>
2021-07-04 12:35:49 +0530

    Added Comments. :)

--- Image Gallery Demo/ImageGallery.cs
+++ Image Gallery Demo/ImageGallery.cs
@@ -21,18 +21,21 @@ namespace Image_Gallery_Demo
     public partial class Image_Gallery_Demo : Form
     {
 
-        public SplitContainer splitContainer1 = new SplitContainer();
-        public TableLayoutPanel tableLayoutPanel1 = new TableLayoutPanel();
-        public Panel panel1 = new Panel();
-        public Panel panel2 = new Panel();
-        public Panel panel3 = new Panel();
-        public TextBox SearchBoxtxt = new TextBox();
+        public SplitContainer C1Split = new SplitContainer();
+        public TableLayoutPanel panelTableLayout = new TableLayoutPanel();
+        public Panel panelImageGallery1 = new Panel();
+        public Panel panelImageGallery2 = new Panel();
+        public Panel panelImageGallery3 = new Panel();
+
+        public TextBox txtSearchBox = new TextBox();
         public PictureBox SearchImageIcon = new PictureBox();
         public PictureBox DownloadImageIcon = new PictureBox();
+
         public PictureBox ExporttoPdfIcon = new PictureBox();
-        public C1.Win.C1Tile.C1TileControl tileControl1 = new C1.Win.C1Tile.C1TileControl();
+        public C1.Win.C1Tile.C1TileControl tileControl = new C1.Win.C1Tile.C1TileControl();
         public C1.Win.C1Tile.Group group1 = new C1.Win.C1Tile.Group();
-        public C1.C1Pdf.C1PdfDocument pdfDocument1 = new C1.C1Pdf.C1PdfDocument();
+        public C1.C1Pdf.C1PdfDocument pdfDocument = new C1.C1Pdf.C1PdfDocument();
+
         public StatusStrip statusStrip1 = new System.Windows.Forms.StatusStrip();
         public ToolStripProgressBar progressBar1 = new System.Windows.Forms.ToolStripProgressBar();
         public C1.Win.C1Tile.Tile img1 = new Tile();
@@ -48,59 +51,71 @@ namespace Image_Gallery_Demo
         {
             void controls1()
             {
+                // UI controls dynamically.
+
+               //Adding Main Form 
                 this.Text = "Image Gallery";
                 this.MaximizeBox = false;
-                this.Size = new System.Drawing.Size(780, 780);
+                this.Size = new System.Drawing.Size(780, 800);
                 this.MaximumSize = new System.Drawing.Size(810, 810);
                 this.ShowIcon = false;
-                this.Controls.Add(splitContainer1);
+                this.Controls.Add(C1Split);
 
-                this.splitContainer1.Dock = DockStyle.Fill;
-                this.splitContainer1.Panel1.Show();
-                this.splitContainer1.Panel2.Show();
-                this.splitContainer1.SplitterDistance = 40;
-                this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
-                this.splitContainer1.Visible = true;
-                this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
-                this.splitContainer1.Margin = new System.Windows.Forms.Padding(2);
-                this.splitContainer1.IsSplitterFixed = true;
-                this.splitContainer1.Panel1.Controls.Add(tableLayoutPanel1);
-                this.splitContainer1.Panel2.Controls.Add(tileControl1);
-                this.splitContainer1.Panel2.Controls.Add(this.statusStrip1);
+                // Adding SplitContainer 
 
-                this.tableLayoutPanel1.ColumnCount = 3;
-                this.tableLayoutPanel1.Dock = DockStyle.Fill;
-                this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-                this.tableLayoutPanel1.RowCount = 1;
-                this.tableLayoutPanel1.Size = new System.Drawing.Size(800, 40);
-                this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-                this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
-                this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
-                this.tableLayoutPanel1.Controls.Add(panel1, 1, 0);
-                this.tableLayoutPanel1.Controls.Add(panel2, 2, 0);
-                this.tableLayoutPanel1.Controls.Add(panel3, 0, 0);
+                this.C1Split.Dock = DockStyle.Fill;
+                this.C1Split.Panel1.Show();
+                this.C1Split.Panel2.Show();
+                this.C1Split.SplitterDistance = 40;
+                this.C1Split.Orientation = System.Windows.Forms.Orientation.Horizontal;
+                this.C1Split.Visible = true;
+                this.C1Split.FixedPanel = System.Windows.Forms.FixedPanel.Panel1;
+                this.C1Split.Margin = new System.Windows.Forms.Padding(2);
+                this.C1Split.IsSplitterFixed = true;
+                this.C1Split.Panel1.Controls.Add(panelTableLayout);
+                this.C1Split.Panel2.Controls.Add(tileControl);
+                this.C1Split.Panel2.Controls.Add(this.statusStrip1);
 
-                this.panel1.Location = new System.Drawing.Point(477, 0);
-                this.panel1.Size = new System.Drawing.Size(287, 40);
-                this.panel1.Dock = DockStyle.Fill;
-                this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.OnSearchPanelPaint);
-                this.panel1.Controls.Add(SearchBoxtxt);
+                //Adding Table Layout Control
 
-                this.SearchBoxtxt.Name = "_searchBox";
-                this.SearchBoxtxt.BorderStyle = 0;
-                this.SearchBoxtxt.Dock = DockStyle.Fill;
-                this.SearchBoxtxt.Location = new System.Drawing.Point(16, 9);
-                this.SearchBoxtxt.Size = new System.Drawing.Size(244, 16);
-                this.SearchBoxtxt.Text = "Search Image";
-                this.SearchBoxtxt.Anchor = (AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right);
+                this.panelTableLayout.ColumnCount = 3;
+                this.panelTableLayout.Dock = DockStyle.Fill;
+                this.panelTableLayout.Location = new System.Drawing.Point(0, 0);
+                this.panelTableLayout.RowCount = 1;
+                this.panelTableLayout.Size = new System.Drawing.Size(800, 40);
+                this.panelTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
+                this.panelTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+                this.panelTableLayout.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 37.5F));
+
+                //Add Pannels1,2,3 to TableLayout to col 1 , 2 , 3.
+                this.panelTableLayout.Controls.Add(panelImageGallery1, 1, 0);
+                this.panelTableLayout.Controls.Add(panelImageGallery2, 2, 0);
+                this.panelTableLayout.Controls.Add(panelImageGallery3, 0, 0);
+
+                this.panelImageGallery1.Location = new System.Drawing.Point(477, 0);
+                this.panelImageGallery1.Size = new System.Drawing.Size(287, 40);
+                this.panelImageGallery1.Dock = DockStyle.Fill;
+                this.panelImageGallery1.Paint += new System.Windows.Forms.PaintEventHandler(this.OnSearchPanelPaint);
+                this.panelImageGallery1.Controls.Add(txtSearchBox);
+
+                //Adding SEarBox to enter Text to search.
+                this.txtSearchBox.Name = "_searchBox";
+                this.txtSearchBox.BorderStyle = 0;
+                this.txtSearchBox.Dock = DockStyle.Fill;
+                this.txtSearchBox.Location = new System.Drawing.Point(16, 9);
+                this.txtSearchBox.Size = new System.Drawing.Size(244, 16);
+                this.txtSearchBox.Text = "Search Image";
+                this.txtSearchBox.Anchor = (AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right);
 
 
-                this.panel2.Location = new System.Drawing.Point(479, 12);
-                this.panel2.Margin = new System.Windows.Forms.Padding(2, 12, 45, 12);
-                this.panel2.Size = new System.Drawing.Size(40, 16);
-                this.panel2.TabIndex = 1;
-                this.panel2.Controls.Add(SearchImageIcon);
 
+                this.panelImageGallery2.Location = new System.Drawing.Point(479, 12);
+                this.panelImageGallery2.Margin = new System.Windows.Forms.Padding(2, 12, 45, 12);
+                this.panelImageGallery2.Size = new System.Drawing.Size(40, 16);
+                this.panelImageGallery2.TabIndex = 1;
+                this.panelImageGallery2.Controls.Add(SearchImageIcon);
+
+                //Adding Search Item.
                 this.SearchImageIcon.Name = "_search";
                 this.SearchImageIcon.Image = global::Image_Gallery_Demo.Properties.Resources.searchicon;
                 this.SearchImageIcon.Dock = DockStyle.Left;
@@ -113,12 +128,13 @@ namespace Image_Gallery_Demo
                 this.SearchImageIcon.Click += new System.EventHandler(this.searchimg_Click);
 
 
-                this.panel3.Dock = DockStyle.Fill;
-                this.panel3.TabIndex = 1;
-                this.panel3.Controls.Add(ExporttoPdfIcon);
-                this.panel3.Controls.Add(DownloadImageIcon);
-                this.panel3.TabIndex = 2;
+                this.panelImageGallery3.Dock = DockStyle.Fill;
+                this.panelImageGallery3.TabIndex = 1;
+                this.panelImageGallery3.Controls.Add(ExporttoPdfIcon);
+                this.panelImageGallery3.Controls.Add(DownloadImageIcon);
+                this.panelImageGallery3.TabIndex = 2;
 
+                //Adding Export Image Button.
                 this.ExporttoPdfIcon.Name = "_exportImage";
                 this.ExporttoPdfIcon.Image = global::Image_Gallery_Demo.Properties.Resources.expdf;
                 this.ExporttoPdfIcon.Location = new System.Drawing.Point(0, 3);
@@ -129,6 +145,7 @@ namespace Image_Gallery_Demo
                 this.ExporttoPdfIcon.BorderStyle = BorderStyle.FixedSingle;
                 this.ExporttoPdfIcon.Paint += new System.Windows.Forms.PaintEventHandler(this.OnExportImagePaint);
 
+                //Adding Download Image.
                 this.DownloadImageIcon.Name = "_DownloadImage";
                 this.DownloadImageIcon.Image = global::Image_Gallery_Demo.Properties.Resources.downbad;
                 this.DownloadImageIcon.Location = new System.Drawing.Point(105, 3);
@@ -139,21 +156,21 @@ namespace Image_Gallery_Demo
                 this.DownloadImageIcon.BorderStyle = BorderStyle.FixedSingle;
 
 
-                this.tileControl1.CellHeight = 78;
-                this.tileControl1.CellSpacing = 11;
-                this.tileControl1.CellWidth = 78;
-                this.tileControl1.Name = "tileControl1";
-                this.tileControl1.Dock = DockStyle.Fill;
-                this.tileControl1.Size = new System.Drawing.Size(764, 718);
-                this.tileControl1.SurfacePadding = new System.Windows.Forms.Padding(12, 4, 12, 4);
-                this.tileControl1.SwipeDistance = 20;
-                this.tileControl1.SwipeRearrangeDistance = 98;
-                this.tileControl1.Groups.Add(this.group1);
-                this.tileControl1.TileChecked += new System.EventHandler<C1.Win.C1Tile.TileEventArgs>(this.OnTileChecked);
-                this.tileControl1.TileUnchecked += new System.EventHandler<C1.Win.C1Tile.TileEventArgs>(this.OnTileUnchecked);
-                this.tileControl1.Paint += new System.Windows.Forms.PaintEventHandler(this.OnTileControlPaint);
-                this.tileControl1.AllowChecking = true;
-                this.tileControl1.Orientation = LayoutOrientation.Vertical;
+                this.tileControl.CellHeight = 78;
+                this.tileControl.CellSpacing = 11;
+                this.tileControl.CellWidth = 78;
+                this.tileControl.Name = "tileControl1";
+                this.tileControl.Dock = DockStyle.Fill;
+                this.tileControl.Size = new System.Drawing.Size(764, 718);
+                this.tileControl.SurfacePadding = new System.Windows.Forms.Padding(12, 4, 12, 4);
+                this.tileControl.SwipeDistance = 20;
+                this.tileControl.SwipeRearrangeDistance = 98;
+                this.tileControl.Groups.Add(this.group1);
+                this.tileControl.TileChecked += new System.EventHandler<C1.Win.C1Tile.TileEventArgs>(this.OnTileChecked);
+                this.tileControl.TileUnchecked += new System.EventHandler<C1.Win.C1Tile.TileEventArgs>(this.OnTileUnchecked);
+                this.tileControl.Paint += new System.Windows.Forms.PaintEventHandler(this.OnTileControlPaint);
+                this.tileControl.AllowChecking = true;
+                this.tileControl.Orientation = LayoutOrientation.Vertical;
 
                 this.statusStrip1.Visible = false;
                 this.statusStrip1.Dock = DockStyle.Bottom;
@@ -183,7 +200,7 @@ namespace Image_Gallery_Demo
 
             statusStrip1.Visible = true;
             imagesList1 = await
-            datafetch1.GetImageData(SearchBoxtxt.Text);
+            datafetch1.GetImageData(txtSearchBox.Text);
             AddTiles(imagesList1);
             statusStrip1.Visible = false;
 
@@ -191,14 +208,14 @@ namespace Image_Gallery_Demo
         //Loop through all the images and add it to the tile control
         private void AddTiles(List<ImageItem> imageList1)
         {
-            tileControl1.Groups[0].Tiles.Clear();
+            tileControl.Groups[0].Tiles.Clear();
 
             foreach (var imageitem in imageList1)
             {
                 Tile tile = new Tile();
                 tile.HorizontalSize = 2;
                 tile.VerticalSize = 2;
-                tileControl1.Groups[0].Tiles.Add(tile);
+                tileControl.Groups[0].Tiles.Add(tile);
 
                 //Converts the base64 encoding to the corresponding image using MemoryStream class.
                 Image img = Image.FromStream(new MemoryStream(imageitem.Base64));
@@ -216,7 +233,7 @@ namespace Image_Gallery_Demo
         private void OnExportClick(object sender, EventArgs e)
         {
             List<Image> images = new List<Image>();
-            foreach (Tile tile in tileControl1.Groups[0].Tiles)
+            foreach (Tile tile in tileControl.Groups[0].Tiles)
             {
                 if (tile.Checked)
                 {
@@ -231,7 +248,7 @@ namespace Image_Gallery_Demo
             if (saveFile.ShowDialog() == DialogResult.OK)
             {
 
-                pdfDocument1.Save(saveFile.FileName);
+                pdfDocument.Save(saveFile.FileName);
 
             }
 
@@ -241,7 +258,7 @@ namespace Image_Gallery_Demo
         private void DownloadImage_Click(object sender, EventArgs e)
         {
             List<Image> images = new List<Image>();
-            foreach (Tile tile in tileControl1.Groups[0].Tiles)
+            foreach (Tile tile in tileControl.Groups[0].Tiles)
             {
                 if (tile.Checked)
                 {
@@ -284,18 +301,18 @@ namespace Image_Gallery_Demo
         //iterates through all the tiles, gets it’s image and convert this list of images to PDF.
         private void ConvertToPdf(List<Image> images)
         {
-            RectangleF rect = pdfDocument1.PageRectangle;
+            RectangleF rect = pdfDocument.PageRectangle;
             bool firstPage = true;
             foreach (var selectedimg in images)
             {
                 if (!firstPage)
                 {
-                    pdfDocument1.NewPage();
+                    pdfDocument.NewPage();
                 }
                 firstPage = false;
                 rect.Inflate(-72, -72);
                 // opens a SaveFileDialog to save the image.
-                pdfDocument1.DrawImage(selectedimg, rect);
+                pdfDocument.DrawImage(selectedimg, rect);
             }
 
         }
@@ -303,7 +320,7 @@ namespace Image_Gallery_Demo
         //To add a grey border to the search box.
         public void OnSearchPanelPaint(object sender, System.Windows.Forms.PaintEventArgs e)
         {
-            Rectangle r = SearchBoxtxt.Bounds;
+            Rectangle r = txtSearchBox.Bounds;
             r.Inflate(3, 3);
             Pen p = new Pen(Color.LightGray);
             e.Graphics.DrawRectangle(p, r);
